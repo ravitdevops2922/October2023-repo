ed929fe4ff99638aaa14612885e7a228d582434a
ravitdevops2922 <ravitdevops2922@gmail.com>
2023-10-12 22:45:08 -0500

    2nd commit

---
+++ file4.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+class prg
+{
+public static void main()
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter your name :");
+String st=s.nextLine();
+System.out.println("Enter your age :");
+int age=s.nextInt();
+System.out.println("Enter your year of birth :");
+String yr=s.nextLine();
+System.out.println("Name :"+st);
+System.out.println("Age :"+age);
+System.out.println("Year of birth :"+yr);
+}
+}
